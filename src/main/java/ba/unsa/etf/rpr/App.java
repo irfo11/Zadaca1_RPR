0a6eb046cc7b3221082b0b2fb75174a11a7e20cc
irfo1 <irfosabanovic@gmail.com>
2022-11-17 23:01:33 +0100

    added javadoc

--- src/main/java/ba/unsa/etf/rpr/App.java
+++ src/main/java/ba/unsa/etf/rpr/App.java
@@ -1,6 +1,13 @@
 package ba.unsa.etf.rpr;
 
+/**
+ * Main class that will run the program and allow to enter expression
+ */
 public class App {
+    /**
+     * Writes out the result of the mathematical expression found in <code>args[0]</code>
+     * @param args holds arguments passed from the console
+     */
     public static void main(String[] args){
         try {
             if(args.length == 0){
